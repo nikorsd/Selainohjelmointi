0727588fa333626fe1c619b0994b37d65907f27b
Niko <niko.rehu@outlook.com>
2026-08-18 14:21:27 +0300

    Redesigned navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,33 +1,49 @@
 <template>
-    <nav>
-        <img @click="$router.push('/')" src="../assets/logo_ai.png"></img>
-        <p class="btn btn-primary" @click="$router.push('/')">Home</p>
-        <p class="btn btn-primary" @click="$router.push('/contact')">Contact</p>
-        <p class="btn btn-primary" @click="$router.push('/about')">About</p>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" @click="$router.push('/')">
+                <img src="../assets/logo_ai.png" alt="Bootstrap" height="50">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                <a class="nav-link" :class="{ active: currentRoute === '/' }" @click="$router.push('/')">Koti</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" :class="{ active: currentRoute === '/contact' }" @click="$router.push('/contact')">Ota yhteyttä</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" :class="{ active: currentRoute === '/about' }" @click="$router.push('/about')">Tietoa meistä</a>
+                </li>
+            </ul>
+            </div>
+        </div>
     </nav>
 </template>
 
+<script setup>
+    import { computed } from 'vue';
+    import { useRoute } from 'vue-router';
+
+    const route = useRoute();
+    const currentRoute = computed(() => route.path);
+</script>
+
 <style scoped>
-    nav {
-        gap: 15px;
-        align-items: center;
-        display: flex;
-        background: rgba(30, 30, 30, 0.6);
-        padding: 8px;
-    }
-
-    img {
-        height: 35px;
-        margin-right: auto;
-        border-radius: 5px;
-    }
-
-    img:hover {
+    .navbar-brand img {
+        border-radius: 20px;
         cursor: pointer;
-        scale: 0.95;
     }
 
-    p {
-        margin: 0;
+    .nav-link.active {
+        font-weight: 800 !important;
+    }
+
+    .nav-link {
+        cursor: pointer;
+        transition: font-weight 0.3s ease;
     }
 </style>
