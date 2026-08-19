d33d02908dbbe6bbb77efa91c77eff629924d4c6
Niko <niko.rehu@outlook.com>
2026-08-19 14:21:17 +0300

    Added footer

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
     import Home from './views/Home.vue'
     import Navbar from './components/Navbar.vue'
+    import Footer from  './components/Footer.vue'
 </script>
 
 <template>
     <Navbar class="nav"/>
     <RouterView/>
+    <Footer></Footer>
 </template>
 
 <style scoped>
---
+++ src/components/Footer.vue
@@ -0,0 +1,44 @@
+<template>
+    <body>
+        <div class="main">
+            <hr>
+            <div class="links">
+                <p>© Kotkantien Maalaus- ja Tapetointi</p>
+                <img src="../assets/logo_ai.webp">
+                <a @click="$router.push('/')">Koti</a>
+                <a @click="$router.push('/contact')">Ota Yhteyttä</a>
+                <a @click="$router.push('/about')">Tietoa Meistä</a>
+                <a @click="$router.push('/studio')">Studio</a>
+            </div>
+        </div>
+    </body>
+</template>
+
+<style scoped>
+    body {
+        display: flex;
+        justify-content: center;
+        background-color: rgb(200, 200, 200);
+        padding: 20px;
+    }
+
+    a {
+        cursor: pointer;
+    }
+
+    img {
+        width: 8rem;
+        margin-left: auto;
+        margin-right: auto;
+        border-radius: 20px;
+    }
+
+    .links {
+        display: flex;
+        gap: 10px;
+    }
+
+    .main {
+        width: 90vw;
+    }
+</style>
