0060e2853f47f9fb3e41f379e36dc629d27aa57e
Niko <niko.rehu@outlook.com>
2026-08-19 14:21:41 +0300

    Added about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,35 @@
 <template>
-    <div>
-        <h1>About</h1>
-    </div>
+    <body>
+        <img src="../assets/osao_kotkantie1.webp">
+        <div>
+            <h1>Tietoa meistä</h1>
+            <hr>
+            <!--Written with AI, since this is a School project and not meant to be deployed-->
+            <p>Kotkantien Maalaus- ja Tapetointi on ammattitaitoinen maalaus- ja tapetointialan yritys. Palveluihimme kuuluvat erilaiset sisä- ja ulkomaalaustyöt, tapetoinnit sekä pintojen kunnostukset.</p>
+            <p>Meille tärkeintä on huolellinen työnjälki, laadukkaat materiaalit ja tyytyväinen asiakas. Toteutamme työt asiakkaan toiveiden ja kohteen tarpeiden mukaan aina suunnittelusta viimeistelyyn asti.</p>
+            <p>Panostamme siistiin työskentelyyn, luotettavuuteen ja hyvään asiakaspalveluun. Olipa kyseessä yksittäisen huoneen uudistaminen tai suurempi maalaus- ja tapetointiprojekti, tavoitteemme on tehdä lopputuloksesta kestävä ja viihtyisä.</p>
+            <p>Kotkantien Maalaus- ja Tapetointi – pintaa, johon voit olla tyytyväinen.</p>
+        </div>
+    </body>
 </template>
+
+<style scoped>
+    body {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        gap: 15px;
+        text-align: center;
+        height: 100vh;
+    }
+
+    div {
+        width: 80vw;
+    }
+
+    img {
+        width: 100vw;
+        mask-image: linear-gradient(to bottom, black 60%, transparent 100%);
+        -webkit-mask-image: linear-gradient(to bottom, black 60%, transparent 100%);
+    }
+</style>
