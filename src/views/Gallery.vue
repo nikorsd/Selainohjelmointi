32764cdf6ee8d3271d2288b1ac9d045f074661c7
Niko <niko.rehu@outlook.com>
2026-08-27 14:24:05 +0300

    Gallery base

--- src/views/Gallery.vue
+++ src/views/Gallery.vue
@@ -1,3 +1,28 @@
 <template>
-    <h1>Gallery</h1>
+    <body>
+        <h1>Gallery</h1>
+        <img src="../assets/osao_kotkantie1.webp" alt="">
+        <div class="main-gallery">
+            <img src="../assets/osao_kotkantie1.webp" alt="">
+            <img src="../assets/osao_kotkantie1.webp" alt="">
+            <img src="../assets/osao_kotkantie1.webp" alt="">
+            <img src="../assets/osao_kotkantie1.webp" alt="">
+            <img src="../assets/osao_kotkantie1.webp" alt="">
+        </div>
+    </body>
 </template>
+
+<style scoped>
+    body {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 100vh;
+        height: 100vh;
+    }
+
+    .main-gallery {
+        display: grid;
+        grid-template-columns: auto auto auto;
+    }
+</style>
